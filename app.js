eb1d013b472295e41331b2f95a6076623107af67
Yusin <yunisaden3@gmail.com>
2023-12-30 21:22:56 +0300

    Nameste-React 1.4: Create a nested Html structure using React

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const heading = React.createElement(
-        'h1',
-        {id:"heading"},
-        "Hello from React World")
-console.log(heading)
+//Creating a nested structure in React
+const parent = React.createElement('div',{id:"parent"},
+React.createElement('div',{id:"child"},
+[ //sibling h1 and h2 tag
+React.createElement("h1",{},"im h1 tag"),
+React.createElement("h2",{},"im h2 tag")
+
+]))
+
+console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(parent)
