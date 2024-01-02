7ae985d72e53809d9c939e68b08701f9cae4c56e
Yusin <yunisaden3@gmail.com>
2024-01-02 21:24:48 +0300

    Nameste-React 3.5:React Element, React Functional component, convertion of element to componet and component composition

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement() => JS Object => Html Element (upon rendering)
-
-const heading = React.createElement("h1", { id: "heading" }, "Nameste React");
-
-console.log(heading);
-
-//JSX - Html-like or XML-like syntax
-//it is more developer friendly then React.createElement()
-
-// Parcel transpiles jsx before it reaches js engine or browser using Babel
-// JSX => React.createElement() => JS Object => Html Element (upon rendering)
-const jsxHeading = <h1 id="heading">Nameste React using Jsx</h1>;
-console.log(jsxHeading);
-
-// multiple line Jsx code should be wrapped in parenthesis
-const jsxHeading1 = (
+//React Element
+const jsxHeading = (
   <h1 className="heading" tabIndex="1">
     Nameste React using Jsx
   </h1>
 );
-console.log(jsxHeading1);
+console.log(jsxHeading);
+
+//React Element can be converted into a React component by adding arrow functions 
+const Title  = () => (<h1> Nameste react from  Title  component</h1>)
+
+//React component can either be (1) Class based component (2)Functional Component
+// Functional component - a function that returns a React Element or Jsx code 
+//component composition is also use for reusability
+const  HeadingComponent = () => ( 
+  <div id="container">
+    <Title />               
+    <h1 className="heading" >
+      Nameste React from functional component
+    </h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
