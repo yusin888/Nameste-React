c3f9d0cbd96689801410bf74b512e2bf6c970fe8
Yusin <yunisaden3@gmail.com>
2024-01-02 20:18:32 +0300

    Nameste-React (3.1) -(3.4): Exploring Jsx and how Babel js is used to transpile it

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//Creating a nested structure in React
-const parent = React.createElement('div',{id:"parent"},
-React.createElement('div',{id:"child"},
-[ //sibling h1 and h2 tag
-React.createElement("h1",{},"im h1 tag"),
-React.createElement("h2",{},"im h2 tag")
+// React.createElement() => JS Object => Html Element (upon rendering)
 
-]))
+const heading = React.createElement("h1", { id: "heading" }, "Nameste React");
 
-console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+console.log(heading);
+
+//JSX - Html-like or XML-like syntax
+//it is more developer friendly then React.createElement()
+
+// Parcel transpiles jsx before it reaches js engine or browser using Babel
+// JSX => React.createElement() => JS Object => Html Element (upon rendering)
+const jsxHeading = <h1 id="heading">Nameste React using Jsx</h1>;
+console.log(jsxHeading);
+
+// multiple line Jsx code should be wrapped in parenthesis
+const jsxHeading1 = (
+  <h1 className="heading" tabIndex="1">
+    Nameste React using Jsx
+  </h1>
+);
+console.log(jsxHeading1);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
